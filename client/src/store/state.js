00ec065d5d14389e8bf87ee505ce96cea73f4a50
obitrim <pevangelist516@gmail.com>
2021-04-09 08:37:35 +0000

    added 'products' state

--- client/src/store/state.js
+++ client/src/store/state.js
@@ -1,5 +1,50 @@
 const state = {
-	navOpen: false
+	navOpen: false,
+	products: [
+		{
+			id: '1',
+			imgURL: '',
+			name: 'Product Nameuct Nameuct Name',
+			price: 23,
+			category: 'clothings'
+		},{
+			id: '2',
+			imgURL: '',
+			name: 'Product uct Nameuct Name',
+			price: 203,
+			category: 'shoes'
+		},{
+			id: '3',
+			imgURL: '',
+			name: 'Product Nameuct Name',
+			price: 93,
+			category: 'clothings'
+		},{
+			id: '4',
+			imgURL: '',
+			name: 'Product Name',
+			price: 83,
+			category: 'electronics'
+		},{
+			id: '5',
+			imgURL: '',
+			name: 'Product Name',
+			price: 23,
+			category: 'shoes'
+		},{
+			id: '6',
+			imgURL: '',
+			name: 'Product uct Nameuct Name',
+			price: 203,
+			category: 'shoes'
+		},{
+			id: '7',
+			imgURL: '',
+			name: 'Product Name',
+			price: 83,
+			category: 'electronics'
+		}
+	]
 };
 
 export default state;

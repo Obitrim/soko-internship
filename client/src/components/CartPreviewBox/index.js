8c0eb7aef3f83224c284930082215ff1bce52708
obitrim <pevangelist516@gmail.com>
2021-04-09 08:54:28 +0000

    work in progress

---
+++ client/src/components/CartPreviewBox/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./CartPreviewBox.css";
+
+const Index = (props) => {
+  return (
+    <div className="CartPreview">
+    	<div  className="ProductsNavigation__Header">Cart <span>0</span></div>
+    </div>
+  )
+}
+
+export default Index;
--- client/src/views/ProductDetails/index.js
+++ client/src/views/ProductDetails/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Index = (props) => {
-  return (
-    <div>Product Details</div>
-  )
+	const { id } = useParams();
+	// const product = 
+
+	return (
+		<div>Product Details {id}</div>
+	)
 }
 export default Index;

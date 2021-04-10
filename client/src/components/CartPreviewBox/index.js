3e3ff78183498b7f8a8314646a829a1b3dd0c37b
obitrim <pevangelist516@gmail.com>
2021-04-10 17:48:59 +0000

    added dummy template

--- client/src/components/CartPreviewBox/index.js
+++ client/src/components/CartPreviewBox/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import DissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+
 import "./CartPreviewBox.css";
+import { StoreContext } from '../../store';
 
 const Index = (props) => {
-  return (
-    <div className="CartPreview">
-    	<div  className="ProductsNavigation__Header">Cart <span>0</span></div>
-    </div>
-  )
+	const { store } = useContext(StoreContext);
+
+	function isEmptyCart(){
+		return store.cart.length === 0;
+	}
+
+	return (
+		<div className="CartPreview">
+			<div  className="ProductsNavigation__Header">Cart <span>{store.cart.length}</span></div>
+			{isEmptyCart()
+				? (
+					<div className="CartPreview__EmptyCart">
+						<h2>Oooops!!!</h2>
+						<DissatisfiedIcon className="DissatisfiedIcon"/>
+						<p>You have no item in your cart.</p>
+					</div>
+				) : (
+					<ul>
+						<li>Item</li>
+						<li>Item</li>
+						<li>Item</li>
+						<li>Item</li>
+					</ul>
+				)}
+			
+			
+		</div>
+	)
 }
 
 export default Index;

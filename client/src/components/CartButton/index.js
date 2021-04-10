b2de447d1a300483321a432dcf55a2dd6b4fe685
obitrim <pevangelist516@gmail.com>
2021-04-10 17:43:30 +0000

    refactored CartButton component

---
+++ client/src/components/CartButton/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import AddShoppingCart from "@material-ui/icons/AddShoppingCart";
+import RemoveShoppingCartIcon from '@material-ui/icons/RemoveShoppingCart';
+
+import './CartButton.css';
+
+const Index = ({className, onAddToBag, onRemoveFromBag, ...props}) => {
+	const [checked, setChecked] = useState(false);
+
+	function handleClick(evt){
+		setChecked(!checked);
+		// if bag is now checked, it means product was added to bag
+		// so call onAddToBag prop from parent
+		if (!checked) return onAddToBag();
+		onRemoveFromBag(); 
+	}
+
+  return (
+    <button 
+    	type="button" 
+    	className={`AddToBagButton ${checked && 'AddToBagButton--Checked'} ${className}`} 
+    	onClick={handleClick}
+    	{...props}
+    	>
+    	{checked ? <RemoveShoppingCartIcon/> : <AddShoppingCart/>}
+    	<span className="AddToBagButton__Text">
+    		{checked ? 'Remove from ' : 'Add to '} bag
+    	</span>
+    </button>
+  )
+}
+
+Index.propTypes = {
+	className: PropTypes.string,
+	onAddToBag: PropTypes.func,
+	onRemoveFromBag: PropTypes.func,
+}
+
+Index.defaultProps = {
+	className: '',
+	onAddToBag: () => alert('onAddToBag prop not provided'),
+	onRemoveFromBag: () => alert('onRemoveToBag prop not provided'),
+}
+
+export default Index;

a358cef679f27ca1b0c79096a6b393bee31dd50a
obitrim <pevangelist516@gmail.com>
2021-04-10 19:32:49 +0000

    refactored markup

--- client/src/views/Cart/index.js
+++ client/src/views/Cart/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import HelpOutlined from '@material-ui/icons/HelpOutlined';
+
+import './Cart.css';
+import CartItem from '../../components/CartItem';
+import Container from '../../components/Container';
+import CheckoutTimeline from '../../components/CheckoutTimeline';
 
 const Index = (props) => {
   return (
-    <div>Cart</div>
+    <div className="CartView">
+    	<Container className="Container">
+	    	<div className="CartView__CartSummary">
+	    		<strong>Cart Summary</strong>
+	    		<CartItem/>
+	    		<CartItem/>
+	    		<CartItem/>
+	    		<CartItem/>
+	    		<CartItem/>
+	    		<hr className="CartView__Divider"/>
+	    		<p className="CartView__HelpInfo">
+	    			<HelpOutlined className="Icon"/>
+	    			<span>Need <a href="#">help</a> shipping your product? Contact our <a href="#">hotline</a></span>
+	    		</p>
+	    	</div>
+	    	<div className="CartView__Steps">
+	    		<CheckoutTimeline/>
+	    		{/*Step1: Account Details*/}
+	    		{/*Step2: Delivery Address*/}
+	    		{/*Step3: payment*/}
+	    	</div>
+	    </Container>
+    </div>
   )
 }
 export default Index;
